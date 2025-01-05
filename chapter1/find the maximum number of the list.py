ea66133f5ecfa83d2186005b2023be08b5b5e07e
Vishal Kapade <vishalkapade93@gmail.com>
2025-01-05 14:53:39 +0530

    practice

---
+++ chapter1/find the maximum number of the list.py
@@ -0,0 +1,6 @@
+numberlist=[32,66,33,99,87,123]
+max=numberlist[0]
+for num in numberlist:
+    if max<num:
+        max=num
+print(max)
---
+++ chapter1/find the middle elementin a list.py
@@ -0,0 +1,3 @@
+numberlist=[11,22,33,44,55,66]
+midelement=int((len(numberlist)/2))
+print(numberlist[midelement])
